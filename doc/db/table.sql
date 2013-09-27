11a93935c64c1ec8ae4f473a97c3b58e2d781797
shinohara_wataru <shinohara_wataru@cyberagent.co.jp>
2013-09-27 11:40:22 +0900

    Revert "Revert "db.default.maxPoolSize=100 を使いきったら終わる""
    This reverts commit af8f1000221e8a3f6585e2b45bb92a1b57c158bc.

---
+++ doc/db/table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `log` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT '1997-05-23 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
